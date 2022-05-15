5270dbc8ab9cfe879cba73d53a1f014ae1b14651
AtomikWiWa <30299472+AtomikWiWa@users.noreply.github.com>
2022-05-15 19:27:27 +0200

    Update localize.ts

--- src/localize.ts
+++ src/localize.ts
@@ -5,6 +5,7 @@ import { HomeAssistant } from "custom-card-helpers";
 import * as de from "./translations/de.json";
 import * as el from "./translations/el.json";
 import * as en from "./translations/en.json";
+import * as es from "./translations/es.json";
 import * as fr from "./translations/fr.json";
 import * as it from "./translations/it.json";
 import * as nb from "./translations/nb.json";
